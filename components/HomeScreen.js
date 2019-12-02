f42242fc160dc60680f3ed10e8149fe7dd8be8aa
Brice LESNE <brice@MacBook-Pro-de-Brice.local>
2019-12-02 23:16:04 +0100

    add nutriScore bullet and moment.js

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,20 +1,41 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Modal, Image } from "react-native";
+import { View, Text, Modal, Image, AsyncStorage } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 import ScanScreen from "./ScanScreen";
 import axios from "axios";
+import moment from "moment";
+import "moment/locale/fr";
 
 const HomeScreen = () => {
   const [isVisible, setIsVisible] = useState(false);
   const [barCode, setBarCode] = useState(null);
   const [data, setData] = useState({});
 
+  moment.locale("fr");
+  let m = moment(new Date(), "YYYY-MM-DD");
+
+  renderNutriColor = () => {
+    if (data.nutriscore_score >= 75) {
+      return "green";
+    } else if (data.nutriscore_score < 25) {
+      return "red";
+    } else if (data.nutriscore_score >= 25 && data.nutriscore_score < 50) {
+      return "orange";
+    } else if (data.nutriscore_score >= 50 && data.nutriscore_score < 75) {
+      return "yellow";
+    }
+  };
+
+  let color = renderNutriColor();
+
   getData = async () => {
     try {
       const response = await axios.get(
         `https://world.openfoodfacts.org/api/v0/product/${barCode}.json`
       );
-      console.log(response.data.product.image_front_url);
+
       setData(response.data.product);
     } catch (error) {
       console.log(error.message);
@@ -22,7 +43,7 @@ const HomeScreen = () => {
   };
 
   useEffect(() => {
-    getData();
+    barCode && getData();
   }, [barCode]);
 
   return (
@@ -44,12 +65,19 @@ const HomeScreen = () => {
         <Text>Go to scan</Text>
       </TouchableOpacity>
       {data.image_front_url && (
-        <Image
-          style={{ width: 150, height: 200 }}
-          source={{
-            uri: data.image_front_url
-          }}
-        />
+        <>
+          <Text>{data.product_name}</Text>
+          <Text>{data.stores_tags[0]}</Text>
+          <Text>{data.nutriscore_score}</Text>
+          <MaterialCommunityIcons name="checkbox-blank-circle" color={color} />
+          <Text>{m.fromNow()}</Text>
+          <Image
+            style={{ width: 150, height: 200 }}
+            source={{
+              uri: data.image_front_url
+            }}
+          />
+        </>
       )}
     </View>
   );
